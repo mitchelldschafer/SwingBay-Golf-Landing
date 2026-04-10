95c3c78e220f02ced5504a3a1cc02eaebb52ed98
Mitchell Schafer <mitchell@example.com>
2026-04-10 11:26:04 -0600

    fix: Express 5 wildcard route syntax - use /{0,} instead of * for catch-all

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.get('/api/health', (_req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
   
-  app.get('*', (req, res) => {
+  app.get('/{0,}', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist', 'index.html'));
   });
 }
